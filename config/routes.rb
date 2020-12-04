266b746a5315a82f9dd4c5e43c17362b98bf79c2
misatol8k <misatolkurihara@gmail.com>
2020-12-04 13:59:20 +0900

    DB作成とブログ機能を追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/blogs', to: 'blogs#index' #追記する
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
 end
